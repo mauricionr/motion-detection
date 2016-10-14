fc97e0ef2a1fffe105193e1751fd9a8a3575e2db
mauricionr <mauricioskateboard@gmail.com>
2016-10-14 11:49:54 +0000

    up

--- motion_detector.py
+++ motion_detector.py
@@ -103,14 +103,14 @@ while True:
 	#cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
 	# show the frame and record if the user presses a key
-	cv2.imshow("Security Feed", frame)
-	cv2.imshow("Thresh", thresh)
-	cv2.imshow("Frame Delta", frameDelta)
+	#cv2.imshow("Security Feed", frame)
+	#cv2.imshow("Thresh", thresh)
+	#cv2.imshow("Frame Delta", frameDelta)
 	key = cv2.waitKey(1) & 0xFF
 
 	# if the `q` key is pressed, break from the lop
-	#if key == ord("q"):
-	#	break
+	if key == ord("q"):
+		break
 
 # cleanup the camera and close any open windows
 camera.release()
