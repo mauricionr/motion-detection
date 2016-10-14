e837fd7e060247b17ad8702ebe80ecd934fff7eb
mauricionr <mauricioskateboard@gmail.com>
2016-10-14 13:56:51 +0000

    up

--- motion_detector.py
+++ motion_detector.py
@@ -9,30 +9,24 @@ import imutils
 import time
 import cv2
 import telegram
+from picamera.array import PiRGBArray
 from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera();
-
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
 bot = telegram.Bot(token='231560685:AAECxkBW-logZ2uJmvXKmeVQlg5tuhG1l8o')
 counter = 0
 limit = 500
+
+time.sleep(0.1)
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
 ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 args = vars(ap.parse_args())
-
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
-	camera = cv2.VideoCapture(0)
-	time.sleep(0.25)
-
-# otherwise, we are reading from a video file
-else:
-	camera = cv2.VideoCapture(args["video"])
 
 # initialize the first frame in the video stream
 firstFrame = None
@@ -103,14 +97,14 @@ while True:
 	#cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
 	# show the frame and record if the user presses a key
-	#cv2.imshow("Security Feed", frame)
-	#cv2.imshow("Thresh", thresh)
-	#cv2.imshow("Frame Delta", frameDelta)
+	cv2.imshow("Security Feed", frame)
+	cv2.imshow("Thresh", thresh)
+	cv2.imshow("Frame Delta", frameDelta)
 	key = cv2.waitKey(1) & 0xFF
 
 	# if the `q` key is pressed, break from the lop
-	if key == ord("q"):
-		break
+	#if key == ord("q"):
+	#	break
 
 # cleanup the camera and close any open windows
 camera.release()
